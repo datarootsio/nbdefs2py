cae8dd5e503bedeae98267dc6e63ef6e5c6e5b2d
Murilo Cunha <murilo@dataroots.io>
2023-01-13 14:16:22 +0100

    add config and funcs to extract functions from string (+tests)

---
+++ nbfuncs/extract.py
@@ -0,0 +1,56 @@
+"""Extract functions from notebooks."""
+from __future__ import annotations
+
+import ast
+from dataclasses import asdict, dataclass
+
+
+@dataclass
+class SubStrBound:
+    """Bounds of a string to be extracted."""
+
+    start_ln: int
+    end_ln: int
+    start_col: int
+    end_col: int
+
+
+class FuncExtractorStr(ast.NodeVisitor):
+    """AST node visitor to extract functions from arbitrary text."""
+
+    def __init__(
+        self: FuncExtractorStr,
+        src: str,
+    ) -> None:
+        """Add empty bounds - find when visiting AST tree."""
+        self.func_bounds: list[SubStrBound] = []
+        self.src = src
+
+    @staticmethod
+    def _substr(
+        s: str, *, start_ln: int, end_ln: int, start_col: int, end_col: int
+    ) -> str:
+        """Get a substring from start and end lines and columns."""
+        lines = s.splitlines()[start_ln - 1 : end_ln]
+        lines[0] = lines[0][start_col:]
+        lines[-1] = lines[-1][:end_col]
+        return "\n".join(lines)
+
+    def visit_FunctionDef(  # noqa: N802
+        self: FuncExtractorStr, node: ast.FunctionDef
+    ) -> ast.FunctionDef:
+        """Add function string bounds to `self.func_bounds`."""
+        self.func_bounds.append(
+            SubStrBound(
+                start_ln=node.lineno,
+                end_ln=node.end_lineno or -1,
+                start_col=node.col_offset,
+                end_col=node.end_col_offset or -1,
+            )
+        )
+        return node
+
+    def funcs(self: FuncExtractorStr) -> list[str]:
+        """Extract the function defitions from the rest of the string."""
+        self.visit(ast.parse(self.src))
+        return [self._substr(self.src, **asdict(bound)) for bound in self.func_bounds]
