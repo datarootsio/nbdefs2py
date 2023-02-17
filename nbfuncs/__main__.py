a226adf947adc2ab27fcd4ce31fcb2cb3faba6a9
Murilo Cunha <murilo@dataroots.io>
2023-02-17 15:16:07 +0100

    refactor cli to make testing easier

--- nbfuncs/__main__.py
+++ nbfuncs/__main__.py
@@ -1,4 +1,6 @@
 """CLI utilities."""
+from __future__ import annotations
+
 import argparse
 
 from nbfuncs.io import export
@@ -9,7 +11,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("source", metavar="SRC", type=str, help="source file/path")
 parser.add_argument("destination", metavar="DST", type=str, help="target file/path")
 parser.add_argument(
-    "-i", "--ignore", type=str, help="glob expression of files to ignore"
+    "-i", "--ignore", type=str, default=None, help="glob expression of files to ignore"
 )
 
 parser.add_argument(
@@ -28,6 +30,13 @@ parser.add_argument(
     "--exclude", type=str, nargs="+", default=None, help="names of functions to ignore"
 )
 
-parser.set_defaults(update=None)
-args = parser.parse_args()
-export(*args)
+
+def main(args: list[str] | None = None) -> None:
+    """Execute from CLI arguments."""
+    parser.set_defaults(update=None)
+    parsed_args = parser.parse_args(args)
+    export(**vars(parsed_args))
+
+
+if __name__ == "__main__":
+    main()
